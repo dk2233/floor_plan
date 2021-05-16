597f5ef3b75a35f6de30b0e6c407ba954c6e85ac
danielku <kucharski22daniel@gmail.com>
2021-05-16 11:02:39 +0200

    init commit

---
+++ floor.py
@@ -0,0 +1,50 @@
+lp1 = 72.5
+ap1 = 13.0
+distance = 1.2
+
+a1 = 208.0
+#length of room from door
+l1 = 462.0
+
+
+class OneSection:
+    def __init__(self,name, dx, dy, distance_from_wall, first_x_shorter , first_y_short = 0, not_whole_room_y = False):
+        print("class of section ",name)
+        self.nr_of_plank = (dx - distance_from_wall*2 - first_x_shorter) / lp1
+
+        self.nr_of_x_part = (dx - distance_from_wall*2- first_x_shorter) % lp1 
+        print("how many in l1 ", self.nr_of_plank)
+
+        if not_whole_room_y is not True: 
+            y_m = 1
+        else:
+            y_m = 2
+            
+        print(" last one ", self.nr_of_x_part) 
+        if first_x_shorter != 0:
+            print(" first one shorter ", first_x_shorter)
+
+        self.how_many_rows = (dy - distance_from_wall * y_m - first_y_short) / ap1
+        self.last_row = (dy - distance_from_wall * y_m - first_y_short) % ap1 
+        if (first_y_short != 0):
+            self.how_many_rows +=1.0
+
+
+        
+
+        print(" how many rows ",self.how_many_rows)
+        print(" last row size ",self.last_row)
+
+
+
+
+s1 = OneSection("near door", l1, a1, distance, 0)
+#s1a = OneSection("near door", l1, a1, distance, 72.5 - 24.0, 0)
+
+sfull = OneSection("full", l1, 560, distance, 0, 0, True)
+#sfull = OneSection("full 2", l1, 560, distance, 0, 10, True)
+
+
+
+
+
